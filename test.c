4f5854b29cf26b93688b81a0af94ff552218679c
huohgj <77164156+huohgj@users.noreply.github.com>
2021-01-13 16:49:48 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,59 +1,64 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
 #include <stdio.h>
-//int max(int x,int y)
+//static 修饰局部变量时，局部变量的生命是周期变长
+//意思就是本来不用static修饰的话每过一次循环test函数里面的a都会因为出了生命周期而还给系统，再一次循环就会重现创一个a
+//a会被刷新，但是加上static就会把之前的值给累计保留下来。然再运行a++
+//static修饰全局变量的时候，会改变变量的作用域，让他只能在自己原来的源文件下使用，不能共享出其他的文件里面用
+//static修饰函数跟修饰全局变量一样不能共享出去用。改变了函数的链接属性
+//外部链接属性-》 内部链接属性
+//extern:声明外部函数所用的东西。
+//extern int add(int int);直接在表示函数里面的是整数还是什么。
+//void test()
+//{
+//	static int a = 1;
+//	a++;
+//		printf("%d\n",a);
+//}
+//#define MAX 10
+//定义标识常量
+//可以用来定义宏-带参数的
+//#define MAX(x,y) (x>y?x:y)
+//int add (int x ,int y)
 //{
 //	if(x>y)
 //		return x;
 //	else
 //		return y;
-//		
+//
 //}
-int main()
+int main ()
 {
-	//条件操作符
-	int a  = 10;
-	int b = 20;
-	int add = 0;
-	add (a>b? a:b);
-	//a是否大于b，是的话他会输出a，不是的话他会输出b
-	
+
+	int a = 10;
+	//&a;//&为取地址符
+	int* p = &a;//有一种变量是用来存放地址的 - 指针变量 int*是用来表示地址的。
+	*p = 20;//* - 解引用操作符。可以用它来表示 a 把a= 10，改为20.
+		printf("%d\n",a);
+	printf("%p\n",&a);
+	//%p是用来打印地址的
 
 
 
 
+	//int a = 10;
+	//int b = 20;
+	//int max = 0;
+	////函数方法
+	//int as = add(a,b);
+	//printf("%d\n",as);
 
-	//逻辑操作符
-	//真 -- 是非零的数都是真的
-	//假 -- 为零
-	//&& -- 逻辑与 ：意思就是两个条件都要是真的最后答案才是真
-	//|| -- 逻辑或 ， 有一个是真的结果就是真的。
-	//显示1就是真的，显示出零意思就是假的。
-	//int a = 3;
-	//int b = 5;
-	//int c = a && b ;
-	///*inr c = a || b ;*/
-	//printf("c = %d\n",c);
+	////宏的方式求哪个大
+	//max = MAX(a,b);
+	//printf ("%d\n",max);
 
 
 
-
-	//1和-1从他存储的地方可以看出来，
-	//[][][][][][][][][][][][][][][][]在最高位上他是1的话那这个数就为负数，为零的话就是正数。
-	//int num1 = 10;
-	//int num2 = 30;
-	//int sum = 0; 
-	//sum = max (num1,num2);
-	//printf("sum=%d\n",sum);
-		
-//	//较大值
-//	int num1 = 10;
-//	int num2 = 40;
-//	if(num1>num2)
-//		printf("最大值：%d\n",num1);
-//	else 
-//	printf("最大值：%d\n",num2);
-		
-	
+	//int i = 0;
+	//while(i<5)
+	//{
+	//	test();
+	//		i++;
+	//}
+	//
 	return 0;
 }
