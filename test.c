3b5abc242bbc4494bc0c78e891882da1065c76ef
huohgj <77164156+huohgj@users.noreply.github.com>
2021-01-15 17:38:20 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,59 +1,60 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
 #include <stdio.h>
-//int max(int x,int y)
-//{
-//	if(x>y)
-//		return x;
-//	else
-//		return y;
-//		
-//}
-int main()
+int main ()
 {
-	//条件操作符
-	int a  = 10;
-	int b = 20;
-	int add = 0;
-	add (a>b? a:b);
-	//a是否大于b，是的话他会输出a，不是的话他会输出b
-	
+	int day = 0;
+	scanf("%d",&day);
 
+	/*switch(day)
+	{
+case 1:
+case 2:
+case 3:
+case 4:
+case 5:
+	printf("工作日\n");
+	break;
+case 6:
+case 7:
+	printf("休息日\n");
+		break;
+	}*/
+	//case为条件进入口，break为结束词
+	//switch(day)
+	//{
+	//case 1:
+	//	printf("星期一\n");
+	//	break;
+	//case 2:
+	//	printf("星期二\n");
+	//	break;
+	//case 3:
+	//	printf("星期三\n");
+	//	break;
+	//case 4:
+	//	printf("星期四\n");
+	//	break;
+	//case 5:
+	//	printf("星期五\n");
+	//	break;
+	//case 6:
+	//	printf("星期六\n");
+	//	break;
+	//case 7:
+	//	printf("星期日\n");
+	//	break;
+	//default:
+	//printf("输入错误");//要是输入了不在范围内的值时弹出的东西比如8、9等等。
+	//}
 
-
-
-
-	//逻辑操作符
-	//真 -- 是非零的数都是真的
-	//假 -- 为零
-	//&& -- 逻辑与 ：意思就是两个条件都要是真的最后答案才是真
-	//|| -- 逻辑或 ， 有一个是真的结果就是真的。
-	//显示1就是真的，显示出零意思就是假的。
-	//int a = 3;
-	//int b = 5;
-	//int c = a && b ;
-	///*inr c = a || b ;*/
-	//printf("c = %d\n",c);
-
-
-
-
-	//1和-1从他存储的地方可以看出来，
-	//[][][][][][][][][][][][][][][][]在最高位上他是1的话那这个数就为负数，为零的话就是正数。
-	//int num1 = 10;
-	//int num2 = 30;
-	//int sum = 0; 
-	//sum = max (num1,num2);
-	//printf("sum=%d\n",sum);
+	////打印出1-100的奇数。
+	//int i =1 ;
+	//while(i<=100)
+	//{
+	//	if(i%2==1)//i商2余1
+	//		printf("%d ",i);
+	//	i++;
+	//}
 		
-//	//较大值
-//	int num1 = 10;
-//	int num2 = 40;
-//	if(num1>num2)
-//		printf("最大值：%d\n",num1);
-//	else 
-//	printf("最大值：%d\n",num2);
-		
-	
 	return 0;
 }
