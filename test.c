ae537017b1a7f1cf1cfb4e9f5ecc7b72b43171ea
huohgj <77164156+huohgj@users.noreply.github.com>
2021-01-18 15:44:19 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,59 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
 #include <stdio.h>
-//int max(int x,int y)
-//{
-//	if(x>y)
-//		return x;
-//	else
-//		return y;
-//		
-//}
 int main()
 {
-	//条件操作符
-	int a  = 10;
-	int b = 20;
-	int add = 0;
-	add (a>b? a:b);
-	//a是否大于b，是的话他会输出a，不是的话他会输出b
-	
+	int i =0;
+	//（初始化；判断；调整）
+	for(i=1;i<=10;i++)
+	{
+		if(5 == i)
+		break;
+		printf("%d\n",i);
+	}
 
 
+	//int ch= 0;
+	//int sb= 0;
+	//char passwork [20] ={0};
+	//printf("请输入密码：");
+	//scanf("%s",passwork);
+	///*getchar();*/
+	//while ((sb = getchar()) != '\n')//如果密码输入123456空格aaaaaa，那我们需要用while循环getchar（）把6后面的都读取掉包括\n
+	//{
+	//		;//这代表不用执行什么命令；
+	//}
+	//
+	////我输入密码123456，scanf只把我输入的123456提取了，因为要按回车所以在缓冲区会留下123456\n，\n==10
+	////ch就会把\n提取，所以会直接进入if语句因为10 ！=1所以会直接判断为放弃确认。我们只需要在前面加个getchar（）；
 
-
-
-	//逻辑操作符
-	//真 -- 是非零的数都是真的
-	//假 -- 为零
-	//&& -- 逻辑与 ：意思就是两个条件都要是真的最后答案才是真
-	//|| -- 逻辑或 ， 有一个是真的结果就是真的。
-	//显示1就是真的，显示出零意思就是假的。
-	//int a = 3;
-	//int b = 5;
-	//int c = a && b ;
-	///*inr c = a || b ;*/
-	//printf("c = %d\n",c);
-
-
-
-
-	//1和-1从他存储的地方可以看出来，
-	//[][][][][][][][][][][][][][][][]在最高位上他是1的话那这个数就为负数，为零的话就是正数。
-	//int num1 = 10;
-	//int num2 = 30;
-	//int sum = 0; 
-	//sum = max (num1,num2);
-	//printf("sum=%d\n",sum);
-		
-//	//较大值
-//	int num1 = 10;
-//	int num2 = 40;
-//	if(num1>num2)
-//		printf("最大值：%d\n",num1);
-//	else 
-//	printf("最大值：%d\n",num2);
-		
-	
-	return 0;
+	//
+	//printf("是否确认(1/0)");
+	//ch= getchar();
+	//if(ch == '1')//记得打字符要打''
+	//{
+	//	printf("登录成功");
+	//}
+	//else
+	//{
+	//		printf("放弃确认");
+	//}
+			return 0;
 }
